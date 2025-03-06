b6d203b453f015c556d314f55e04f0a823e9df2e
mehedimrm22 <mehedi.mrm@gmail.com>
2025-03-07 00:57:12 +0200

    navigation spaces fixed and navigation css file created

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,29 +1,74 @@
 import { useContext, useEffect } from "react";
 import { Link, Outlet } from "react-router-dom";
 import { ThemeContext } from "../context";
+import logo from "../../public/logo.svg";
+import "./Navigation.css";
 
 export default function Navigation() {
-    const {loggedIn, setLoggedIn} = useContext(ThemeContext);
+  const { loggedIn, setLoggedIn } = useContext(ThemeContext);
 
-    useEffect(() => {
-        const token = localStorage.getItem("access_token");
-        setLoggedIn(!!token);
-    }, []);
+  useEffect(() => {
+    const token = localStorage.getItem("access_token");
+    setLoggedIn(!!token);
+  }, []);
 
-    return (
-        <>
-        <div className="sidebar">
-            <button><Link to="register" style={{textDecoration: 'none', color: 'inherit'}}>Register</Link></button>
-            {loggedIn
-            ? <button onClick={() => {
-                localStorage.removeItem('access_token')
-                setLoggedIn(false)
-            }}><Link to="login" style={{textDecoration: 'none', color: 'inherit'}}>Log out</Link></button> 
-            : <button onClick={() => setLoggedIn(true)}><Link to="login" style={{textDecoration: 'none', color: 'inherit'}}>Log in</Link></button>}
-            <button><Link to="file-upload" style={{textDecoration: 'none', color: 'inherit'}}>Upload</Link></button>
-            <button><Link to="all-uploads" style={{textDecoration: 'none', color: 'inherit'}}>All uploads</Link></button>
-        </div>
-        <Outlet />
-        </>
-    )
+  return (
+    <>
+      <div className="sidebar">
+        <Link to="all-uploads">
+          <img src={logo} alt="File Uploader - noobDev" />
+        </Link>
+
+        <button>
+          <Link
+            to="register"
+            style={{ textDecoration: "none", color: "inherit" }}
+          >
+            Register
+          </Link>
+        </button>
+        {loggedIn ? (
+          <button
+            onClick={() => {
+              localStorage.removeItem("access_token");
+              setLoggedIn(false);
+            }}
+          >
+            <Link
+              to="login"
+              style={{ textDecoration: "none", color: "inherit" }}
+            >
+              Log out
+            </Link>
+          </button>
+        ) : (
+          <button onClick={() => setLoggedIn(true)}>
+            <Link
+              to="login"
+              style={{ textDecoration: "none", color: "inherit" }}
+            >
+              Log in
+            </Link>
+          </button>
+        )}
+        <button>
+          <Link
+            to="file-upload"
+            style={{ textDecoration: "none", color: "inherit" }}
+          >
+            Upload
+          </Link>
+        </button>
+        <button>
+          <Link
+            to="all-uploads"
+            style={{ textDecoration: "none", color: "inherit" }}
+          >
+            All uploads
+          </Link>
+        </button>
+      </div>
+      <Outlet />
+    </>
+  );
 }
