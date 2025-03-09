ec369768abaf5ab8769d438614355ff2e835727c
mehedimrm22 <mehedi.mrm@gmail.com>
2025-03-09 21:18:35 +0200

    sidebar css updated

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -15,60 +15,66 @@ export default function Navigation() {
   return (
     <>
       <div className="sidebar">
-        <Link className="sidebar-logo" to="all-uploads">
-          <img src={logo} alt="File Uploader - noobDev" />
-        </Link>
+        <div className="sidebar-logo">
+          <Link className="sidebar-logo" to="all-uploads">
+            <img src={logo} alt="File Uploader - noobDev" />
+          </Link>
+        </div>
 
-        <button>
-          <Link
-            to="register"
-            style={{ textDecoration: "none", color: "inherit" }}
-          >
-            Register
-          </Link>
-        </button>
-        {loggedIn ? (
-          <button
-            onClick={() => {
-              localStorage.removeItem("access_token");
-              setLoggedIn(false);
-            }}
-          >
+        <div className="sidebar-links">
+          <button>
             <Link
-              to="login"
+              to="register"
               style={{ textDecoration: "none", color: "inherit" }}
             >
-              Log out
+              Register
             </Link>
           </button>
-        ) : (
-          <button onClick={() => setLoggedIn(true)}>
+          {loggedIn ? (
+            <button
+              onClick={() => {
+                localStorage.removeItem("access_token");
+                setLoggedIn(false);
+              }}
+            >
+              <Link
+                to="login"
+                style={{ textDecoration: "none", color: "inherit" }}
+              >
+                Log out
+              </Link>
+            </button>
+          ) : (
+            <button onClick={() => setLoggedIn(true)}>
+              <Link
+                to="login"
+                style={{ textDecoration: "none", color: "inherit" }}
+              >
+                Log in
+              </Link>
+            </button>
+          )}
+          <button>
             <Link
-              to="login"
+              to="file-upload"
               style={{ textDecoration: "none", color: "inherit" }}
             >
-              Log in
+              Upload
             </Link>
           </button>
-        )}
-        <button>
-          <Link
-            to="file-upload"
-            style={{ textDecoration: "none", color: "inherit" }}
-          >
-            Upload
-          </Link>
-        </button>
-        <button>
-          <Link
-            to="all-uploads"
-            style={{ textDecoration: "none", color: "inherit" }}
-          >
-            All uploads
-          </Link>
-        </button>
-        <div className="copyright-text">
-          <p>Copyright © 2025 | noobDev</p>
+          <button>
+            <Link
+              to="all-uploads"
+              style={{ textDecoration: "none", color: "inherit" }}
+            >
+              All uploads
+            </Link>
+          </button>
+        </div>
+        <div className="sidebar-footer">
+          <div className="copyright-text">
+            <p>Copyright © 2025 | noobDev</p>
+          </div>
         </div>
       </div>
       <Outlet />
