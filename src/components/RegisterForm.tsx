cbffc767ba4c0f5f5116e4f9f37e189fef3d4454
mehedimrm22 <mehedi.mrm@gmail.com>
2025-03-09 21:31:27 +0200

    register form updated

--- src/components/RegisterForm.tsx
+++ src/components/RegisterForm.tsx
@@ -1,51 +1,86 @@
 import axios from "axios";
 import { useState } from "react";
+import "./RegisterForm.css";
 
 export default function RegisterForm() {
+  const [registerData, setRegisterData] = useState({
+    email: "",
+    password: "",
+    password_confirmation: "",
+    token_name: "",
+  });
+  const [registeredNotification, setRegisteredNotification] = useState("");
 
-    const [registerData, setRegisterData] = useState({
-        email: "",
-        password: "",
-        password_confirmation: "",
-        token_name: ""
-    })
-    const [registeredNotification, setRegisteredNotification] = useState("")
-
-    function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
-        e.preventDefault();
-        axios.post('https://unelmacloud.com/api/v1/auth/register', registerData, {
-            headers: {
-                "Content-Type": "application/json",
-                "Accept": "application/json"
-            }
-        })
-        .then(function(response) { if (response.data.status === 'success') {
-        setRegisteredNotification('You was succesfully registered!')
-         setTimeout(() => {
-            setRegisteredNotification('')
-        }, 5_000)
-        setRegisterData({email: "", password: "", password_confirmation: "", token_name: ""});
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    axios
+      .post("https://unelmacloud.com/api/v1/auth/register", registerData, {
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      })
+      .then(function (response) {
+        if (response.data.status === "success") {
+          setRegisteredNotification("You was succesfully registered!");
+          setTimeout(() => {
+            setRegisteredNotification("");
+          }, 5_000);
+          setRegisterData({
+            email: "",
+            password: "",
+            password_confirmation: "",
+            token_name: "",
+          });
         } else {
-            setRegisteredNotification(`${response.data.message}`)
-            setTimeout(() => {
-               setRegisteredNotification('')
-           }, 5_000)}});
-    }
+          setRegisteredNotification(`${response.data.message}`);
+          setTimeout(() => {
+            setRegisteredNotification("");
+          }, 5_000);
+        }
+      });
+  }
 
-    return (
-        <>
-        <div className="registerForm right-container">
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="email">Email</label>
-                <input type="email" id="email" required onChange={(e) => setRegisterData({...registerData, email: e.target.value, token_name: e.target.value})}></input>
+  return (
+    <>
+      <div className="registerForm right-container">
+        <form onSubmit={handleSubmit}>
+          <div className="email">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              required
+              onChange={(e) =>
+                setRegisterData({
+                  ...registerData,
+                  email: e.target.value,
+                  token_name: e.target.value,
+                })
+              }
+            ></input>
+          </div>
 
-                <label htmlFor="password">Password</label>
-                <input type="password" id="password" required onChange={(e) => setRegisterData({...registerData, password: e.target.value, password_confirmation: e.target.value})}></input>
+          <div className="password">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              required
+              onChange={(e) =>
+                setRegisterData({
+                  ...registerData,
+                  password: e.target.value,
+                  password_confirmation: e.target.value,
+                })
+              }
+            ></input>
+          </div>
 
-                <button type="submit">Register</button>
-            </form>
-        </div>
-        {registeredNotification && registeredNotification}
-        </>
-    )
+          <button type="submit">Register</button>
+        </form>
+      </div>
+      {registeredNotification && registeredNotification}
+    </>
+  );
 }
